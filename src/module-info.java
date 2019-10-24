2dbfc3493cbbf13226c802f05c3fabcde53005f5
MykSto <hushokas@gmail.com>
2019-10-25 02:09:28 +0300

    [001] add: refactor | threads

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,9 @@
-module File.Reader {
+module file.reader {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.desktop;
     requires java.logging;
+
 
     opens sample;
 }
--- src/sample/FileChooserApp.java
+++ src/sample/FileChooserApp.java
@@ -3,8 +3,9 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -12,51 +13,60 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.*;
-import java.util.List;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public final class FileChooserSample extends Application {
+public final class FileChooserApp extends Application {
 
-    private Desktop desktop = Desktop.getDesktop();
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 
     @Override
     public void start(final Stage stage) {
-        stage.setTitle("File Chooser Sample");
+        stage.setTitle("File Reader");
 
         final FileChooser fileChooser = new FileChooser();
+        final Button openMultipleButton = new Button("Upload Files");
 
-        final Button openButton = new Button("Open a Picture...");
-        final Button openMultipleButton = new Button("Open Multiple...");
+        //Graphical Interface
+        final GridPane inputGridPane = new GridPane();
+        GridPane.setConstraints(openMultipleButton, 2,1);
+        inputGridPane.setHgap(60);
+        inputGridPane.setVgap(10);
+        inputGridPane.getChildren().addAll(openMultipleButton);
 
-        openButton.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(final ActionEvent e) {
-                        File file = fileChooser.showOpenDialog(stage);
-                        if (file != null) {
-                            openFile(file);
-                        }
-                    }
-                });
+        final Pane rootGroup = new VBox(12);
+        rootGroup.getChildren().addAll(inputGridPane);
+        stage.setScene(new Scene(rootGroup));
 
+        Canvas canvas = new Canvas(300,50);
+        rootGroup.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.fillText( "Welcome to the File Reader v1.0", 70, 10 );
+        gc.strokeText( "New files will appear in the same file directory", 35, 40 );
+
+        stage.show();
+
+        //File upload functionality
         openMultipleButton.setOnAction(
                 new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(final ActionEvent e) {
+                        //List of uploaded files
                         List<File> list = fileChooser.showOpenMultipleDialog(stage);
                         if (list != null) {
                             String fileFolder = null;
                             Map<String, Integer> map = new HashMap<>();
                             try {
                                 for (File file : list) {
+                                    //Read the files
                                     BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getAbsolutePath()));
                                     bufferedReader.lines().filter(Objects::nonNull).forEach(line -> {
-                                        for (String word : line.trim().split("[\\s@&.?$+-,/:;\"!#%]+")) {
+                                        for (String word : line.trim().split("[\\s@&.?$+-,/:;\"!#%=~<>{}()\\-*\\[\\]\\d]+")) {
                                             String upWord = word.toUpperCase();
                                             if (word.length() != 0 &&
                                                     'A' <= upWord.charAt(0) && upWord.charAt(0) <= 'Z') {
@@ -77,49 +87,28 @@ public final class FileChooserSample extends Application {
                     }
                 });
 
-
-        final GridPane inputGridPane = new GridPane();
-
-        GridPane.setConstraints(openButton, 0, 0);
-        GridPane.setConstraints(openMultipleButton, 1, 0);
-        inputGridPane.setHgap(6);
-        inputGridPane.setVgap(6);
-        inputGridPane.getChildren().addAll(openButton, openMultipleButton);
-
-        final Pane rootGroup = new VBox(12);
-        rootGroup.getChildren().addAll(inputGridPane);
-        rootGroup.setPadding(new Insets(12, 12, 12, 12));
-
-        stage.setScene(new Scene(rootGroup));
-        stage.show();
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-
-    private void openFile(File file) {
-        try {
-            desktop.open(file);
-        } catch (IOException ex) {
-            Logger.getLogger(
-                    FileChooserSample.class.getName()).log(
-                    Level.SEVERE, null, ex
-            );
-        }
-    }
-
+    //Writes new files with the results
     private static void writeFiles(Map<String, Integer> map, String fileFolder) {
         EnumSet.allOf(Letters.class).forEach(e -> {
-            try (FileWriter fw = new FileWriter(fileFolder.concat(e.fileName))) {
-                fw.write(map.entrySet().stream().filter(key -> {
-                    char firstChar = key.getKey().toUpperCase().charAt(0);
-                    return firstChar >= e.startChar && firstChar <= e.finishChar;
-                }).sorted(Comparator.comparing(Map.Entry::getKey))
-                        .map(Object::toString).collect(Collectors.joining("\n")));
-            } catch (Exception y) {
-                y.printStackTrace();
-            }
+            Thread thread = new Thread(){
+                @Override
+                public synchronized void start() {
+                    try (FileWriter fw = new FileWriter(fileFolder.concat(e.fileName))) {
+                        fw.write(map.entrySet().stream().filter(key -> {
+                            char firstChar = key.getKey().toUpperCase().charAt(0);
+                            return firstChar >= e.startChar && firstChar <= e.finishChar;
+                        }).sorted(Comparator.comparing(Map.Entry::getKey))
+                                .map(Object::toString).collect(Collectors.joining("\r\n")));
+                    } catch (Exception y) {
+                        y.printStackTrace();
+                    }
+                    System.out.println(e.name() + " has " + Thread.activeCount() + " threads");
+                }
+            };
+            thread.start();
         });
     }
+
 }
